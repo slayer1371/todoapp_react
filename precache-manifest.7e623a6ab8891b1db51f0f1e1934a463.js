209b0a31e862a535c6c375788be388f618d6f04b
Mrinal <mrinal.sharma2020@vitstudent.ac.in>
2022-06-28 20:58:48 +0530

    Updates

---
+++ precache-manifest.7e623a6ab8891b1db51f0f1e1934a463.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4198f8f99bd7cfa9c645b7066b201cd",
+    "url": "/todoapp_react/index.html"
+  },
+  {
+    "revision": "9302e28affcbd310209f",
+    "url": "/todoapp_react/static/js/2.17e1ab02.chunk.js"
+  },
+  {
+    "revision": "d162f44dfbc07077615a",
+    "url": "/todoapp_react/static/js/main.48b99b23.chunk.js"
+  },
+  {
+    "revision": "1b0943212df7e3b537cd",
+    "url": "/todoapp_react/static/js/runtime-main.d90f0f04.js"
+  }
+]);
